10f34fb6a9fe534bccf16d97111ca45618f870b7
LeeJeongHeon02 <aoc05230@naver.com>
2026-01-08 16:42:49 +0900

    feat : category 관련 기능 개발

--- src/main/java/com/budget_book/budget_book/repository/CategoryRepository.java
+++ src/main/java/com/budget_book/budget_book/repository/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.budget_book.budget_book.repository;
 import com.budget_book.budget_book.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findAllByUser_Name(String name);
 }
+
