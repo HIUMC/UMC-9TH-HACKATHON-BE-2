2e525b85bc5584ddef0993d268653c10e65848d3
seungju <lsjxcs@gmail.com>
2026-01-08 16:21:03 +0900

    임시커밋

---
+++ src/main/java/com/budget_book/budget_book/dto/request/UserRequestDTO.java
@@ -0,0 +1,12 @@
+package com.budget_book.budget_book.dto.request;
+
+import lombok.Getter;
+
+public class UserRequestDTO {
+
+    @Getter
+    public static class LoginDTO {
+        private String nickname;
+        private String password;
+    }
+}
